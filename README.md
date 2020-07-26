21e023f15c27d73c01e596662aad0b5ec3b0ef87
Petr Sima <ptea.sima@gmail.com>
2020-07-26 20:15:36 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # BetterEnvironment
 
-A description of this package.
+SwiftUI sugar.
+
+Lets you subscript by the key type, like this:
+
+```
+struct ContentView: View {
+  @Environment(key: K.self) var profit
+    var body: some View {
+        Text("Hello, world!")
+          .onTapGesture { profit() }
+    }
+}
+```
+
+without the need to define a proxy property, like this:
+
+```
+extension EnvironmentValues {
+  var k: K.Value {
+    get { self[K.self] }
+    set { self[K.self] = newValue }
+  }
+}
+struct ContentView: View {
+  @Environment(\.k) var lessProfit
+    var body: some View {
+        Text("Hello, world!")
+          .onTapGesture { lessProfit() }
+    }
+}
+```
+
+Works with any EvironmentKey and is purely additive.
--- Sources/BetterEnvironment/BetterEnvironment.swift
+++ Sources/BetterEnvironment/BetterEnvironment.swift
@@ -1,3 +1,32 @@
+import SwiftUI
+
+struct Unit: Hashable, Codable { }
+// this is https://github.com/pointfreeco/swift-tagged but I chose not to depend on it.
+struct Tagged<Tag, RawValue> {
+  var rawValue: RawValue
+}
+extension Tagged: Equatable where RawValue: Equatable {}
+extension Tagged: Hashable where RawValue: Hashable {}
+
+extension EnvironmentValues {
+  subscript<Key: EnvironmentKey>(key key: Tagged<Key, Unit>) -> Key.Value {
+    get { self[Key.self] }
+    set { self[Key.self] = newValue }
+  }
+}
+
+extension Environment {
+  init<Key: EnvironmentKey>(key: Key.Type) where Key.Value == Value {
+    self.init(\.[key: Tagged<Key, Unit>(rawValue: Unit())])
+  }
+}
+
+extension View {
+  func environment<Key: EnvironmentKey>(key: Key.Type, value: Key.Value) -> some View {
+    self
+      .environment(\.[key: Tagged<Key, Unit>(rawValue: Unit())], value)
+  }
+}
 struct BetterEnvironment {
     var text = "Hello, World!"
 }
