bb620dcdfed6187edab9286763ecf430400f26ed
Petr Sima <ptea.sima@gmail.com>
2020-07-31 15:33:38 +0200

    simplified by replace Tagged with HashableType

--- Sources/BetterEnvironment/BetterEnvironment.swift
+++ Sources/BetterEnvironment/BetterEnvironment.swift
@@ -1,15 +1,9 @@
 import SwiftUI
 
-struct Unit: Hashable { }
-// this is https://github.com/pointfreeco/swift-tagged but I chose not to depend on it.
-struct Tagged<Tag, RawValue> {
-  var rawValue: RawValue
-}
-extension Tagged: Equatable where RawValue: Equatable {}
-extension Tagged: Hashable where RawValue: Hashable {}
+struct HashableType<T>: Hashable {}
 
 extension EnvironmentValues {
-  subscript<Key: EnvironmentKey>(key key: Tagged<Key, Unit>) -> Key.Value {
+  subscript<Key: EnvironmentKey>(key key: HashableType<Key>) -> Key.Value {
     get { self[Key.self] }
     set { self[Key.self] = newValue }
   }
@@ -17,21 +11,21 @@ extension EnvironmentValues {
 
 public extension Environment {
   init<Key: EnvironmentKey>(key: Key.Type) where Key.Value == Value {
-    self.init(\.[key: Tagged<Key, Unit>(rawValue: Unit())])
+    self.init(\.[key: HashableType<Key>()])
   }
   
   init<Key: EnvironmentKey>(key: Key.Type, selection: WritableKeyPath<Key.Value, Value>) {
-    self.init((\EnvironmentValues.[key: Tagged<Key, Unit>(rawValue: Unit())]).appending(path: selection))
+    self.init((\EnvironmentValues.[key: HashableType<Key>()]).appending(path: selection))
   }
 }
 
 public extension View {
   func environment<Key: EnvironmentKey>(key: Key.Type, value: Key.Value) -> some View {
     self
-      .environment(\.[key: Tagged<Key, Unit>(rawValue: Unit())], value)
+      .environment(\.[key: HashableType<Key>()], value)
   }
   func transformEnvironment<Key: EnvironmentKey>(key: Key.Type, transform: @escaping (inout Key.Value) -> Void) -> some View {
     self
-      .transformEnvironment(\.[key: Tagged<Key, Unit>(rawValue: Unit())], transform: transform)
+      .transformEnvironment(\.[key: HashableType<Key>()], transform: transform)
   }
 }
